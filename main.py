de83efb86d90884d25675a75cb690e9ad03d0a55
sylvieeik <eternalheat.illusion@gmail.com>
2022-12-02 23:56:49 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-client = discord.Client()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
